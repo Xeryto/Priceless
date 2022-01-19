59e548300b2de9fdb20c37c7bd08b36efe16ddce
Xeryto <igoshindanya@mail.ru>
2022-01-19 19:38:30 +0300

    added language switcher

--- Priceless/wwwroot/js/site.js
+++ Priceless/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let ruText = document.querySelectorAll(".ru");
+let enText = document.querySelectorAll(".en");
+enText.forEach(function (item) {
+    item.classList.add("hidden");
+});
 
-// Write your JavaScript code.
+let ruSwitch = document.querySelector("#lngSwitch1");
+let enSwitch = document.querySelector("#lngSwitch2");
+
+ruSwitch.addEventListener("click", function () {
+    ruText.forEach(function (item) {
+        item.classList.remove("hidden");
+    });
+    enText.forEach(function (item) {
+        item.classList.add("hidden");
+    });
+});
+enSwitch.addEventListener("click", function () {
+    enText.forEach(function (item) {
+        item.classList.remove("hidden");
+    });
+    ruText.forEach(function (item) {
+        item.classList.add("hidden");
+    });
+});
