22f02d66eb241c2c20247cb1eaa454957ba9437e
Xeryto <igoshindanya@mail.ru>
2022-02-02 22:43:53 +0300

    translated left pages + made language switcher not reset

--- Priceless/wwwroot/js/site.js
+++ Priceless/wwwroot/js/site.js
@@ -1,11 +1,47 @@
 ﻿let ruText = document.querySelectorAll(".ru");
 let enText = document.querySelectorAll(".en");
-enText.forEach(function (item) {
-    item.classList.add("hidden");
-});
 
 let ruSwitch = document.querySelector("#lngSwitch1");
 let enSwitch = document.querySelector("#lngSwitch2");
+let links = document.querySelectorAll(".linkLng");
+
+function get(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
+
+let curLng = get('lng');
+if (curLng != null) {
+    if (curLng == "en") {
+        ruText.forEach(function (item) {
+            item.classList.add("hidden");
+        });
+        links.forEach(function (link) {
+            let url = new URL(link.href);
+            url.searchParams.set('lng', "en")
+            link.href = url;
+        });
+    } else {
+        enText.forEach(function (item) {
+            item.classList.add("hidden");
+        });
+        links.forEach(function (link) {
+            let url = new URL(link.href);
+            url.searchParams.set('lng', "ru");
+            link.href = url;
+        });
+    }
+}
+else {
+    enText.forEach(function (item) {
+        item.classList.add("hidden");
+    });
+    links.forEach(function (link) {
+        let url = new URL(link.href);
+        url.searchParams.set('lng', "ru");
+        link.href = url;
+    });
+}
 
 ruSwitch.addEventListener("click", function () {
     ruText.forEach(function (item) {
@@ -13,6 +49,11 @@ ruSwitch.addEventListener("click", function () {
     });
     enText.forEach(function (item) {
         item.classList.add("hidden");
+    });
+    links.forEach(function (link) {
+        let url = new URL(link.href);
+        url.searchParams.set('lng', "ru");
+        link.href = url;
     });
 });
 enSwitch.addEventListener("click", function () {
@@ -22,4 +63,11 @@ enSwitch.addEventListener("click", function () {
     ruText.forEach(function (item) {
         item.classList.add("hidden");
     });
+    links.forEach(function (link) {
+        let url = new URL(link.href);
+        url.searchParams.set('lng', "en");
+        link.href = url;
+    });
 });
+
+
